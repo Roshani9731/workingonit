f6c995f992e7731ab964e05760c6f5eebeae80f6
Roshani9731 <roshaninavdiya@gmail.com>
2023-08-10 10:52:16 +0530

    workingonit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:['Frontend Developer','Bug Bounty Hunter', 'Security Enthusisast'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    long: true
+});
