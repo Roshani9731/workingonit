b41bdfee038ddb9b0f9e5f7f92e7647d6f9be67a
Roshani9731 <roshaninavdiya@gmail.com>
2023-10-10 17:13:40 +0530

    New changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings:['Frontend Developer','Bug Bounty Hunter', 'Security Enthusisast'],
+    strings:['Frontend Developer','UI/UX Developer', 'Security Enthusisast'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
